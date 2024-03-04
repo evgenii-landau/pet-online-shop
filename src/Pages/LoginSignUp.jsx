1ea5093e656a5902b748cab180b30d9b0887cced
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-03-04 16:07:00 +0300

    login page

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import styles from './CSS/LoginSignUp.module.scss'
+
 
 export const LoginSignUp = () => {
 	return (
-		<div>LoginSignUp</div>
+		<div className={styles.loginSignUp}>
+			<div className={styles.container}>
+				<h1>Sign Up</h1>
+				<div className={styles.fields}>
+					<input type="text" placeholder='Your Name'/>
+					<input type="email" placeholder='Email Adress'/>
+					<input type="password" placeholder='Password'/>
+				</div>
+				<button type='button'>Continue</button>
+				<p className={styles.login}>Already have an account?</p> <span>Login here</span>
+				<div className={styles.agree}>
+					<input type="checkbox" name='' id=''/>
+					<p>By continuing, i agree to the terms of use & privacy policy.</p>
+				</div>
+			</div>
+		</div>
 	)
 }
