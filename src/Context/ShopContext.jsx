e6e3047e6ec2d8ce93553ca78de1d933dfa7f968
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-03-05 14:42:12 +0300

    added localStorage for cart

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import React, {createContext} from "react";
 import all_product from "../Components/assets/all_product.js";
 
+
 export const ShopContext = createContext(null)
 
 const getDefaultCart = () => {
@@ -16,15 +17,86 @@ const ShopContextProvider = (props) => {
 
 	const addToCart = (itemId) => {
 		setCartItems((prev) => (
-			{...prev, [itemId]: prev[itemId] + 1}
+			{
+				...prev,
+				[itemId]: prev[itemId] + 1
+			}
 		))
-		console.log(cartItems)
+
+		if (!localStorage.getItem('localItems')) {
+			localStorage.setItem('localItems', JSON.stringify([{...all_product[itemId - 1], count: 1}]))
+		} else {
+			let localItems = JSON.parse(localStorage.getItem('localItems'))
+
+			const obj = localItems.find(item => item.id === itemId)
+
+			if (obj) {
+				localItems.map(item => {
+					if (item.id === itemId) {
+						item.count += 1
+						localStorage.setItem('localItems', JSON.stringify(localItems))
+					}
+				})
+			} else {
+				localItems = [...localItems, {...all_product[itemId - 1], count: 1}]
+				localStorage.setItem('localItems', JSON.stringify(localItems))
+			}
+
+			// localItems.find(item => {
+			// 	if (item.id === itemId) {
+			// 		localItems.map(item => {
+			// 			item.count += 1
+			// 			localStorage.setItem('localItems', JSON.stringify(localItems))
+			// 			flag = true
+			// 		})
+			// 	}
+			// 	if (flag === false) {
+			// 		localItems = [...localItems, {...all_product[item - 1], count: 1}]
+			// 		localStorage.setItem('localItems', JSON.stringify(localItems))
+			// 	}
+			// })
+
+
+			// if (JSON.parse(localStorage.getItem('localItems')).find(item => item.id === itemId)) {
+			// 	let localItems = JSON.parse(localStorage.getItem('localItems'))
+			// 	localItems.map(item => {
+			// 		console.log(item[itemId])
+			// 		item.count += 1
+			// 		localStorage.setItem('localItems', JSON.stringify(localItems))
+			// 	})
+			// } else {
+			// 	let localItems = JSON.parse(localStorage.getItem('localItems'))
+			// 	localItems = [...localItems, {...all_product[itemId - 1], count: 1}]
+			// 	localStorage.setItem('localItems', JSON.stringify(localItems))
+			// }
+		}
 	}
 
 	const removeFromCart = (itemId) => {
 		setCartItems((prev) => (
-			{...prev, [itemId]: prev[itemId] - 1}
+			{
+				...prev,
+				[itemId]: prev[itemId] - 1
+			}
 		))
+
+		let localItems = JSON.parse(localStorage.getItem('localItems'))
+		let obj = localItems.find(item => item.id === itemId)
+
+		if (obj.count > 0) {
+			localItems.map(item => {
+				if (item.id === itemId) {
+					if (item.count > 0) {
+						item.count -= 1
+					}
+				}
+			})
+		}
+		if (obj.count === 0 ) {
+			localItems = localItems.filter(item => item.id !== itemId)
+		}
+
+		localStorage.setItem('localItems', JSON.stringify(localItems))
 	}
 
 	const getTotalCartAmount = () => {
@@ -42,15 +114,27 @@ const ShopContextProvider = (props) => {
 	const getTotalCardItems = () => {
 		let totalItems = 0
 
-		for (const item in cartItems) {
-			if (cartItems[item] > 0) {
-				totalItems += cartItems[item]
-			}
+		// for (const item in cartItems) {
+		// 	if (cartItems[item] > 0) {
+		// 		totalItems += cartItems[item]
+		// 	}
+		// }
+		// return totalItems
+		if (localStorage.getItem('localItems')) {
+			totalItems = JSON.parse(localStorage.getItem('localItems')).length
 		}
+
 		return totalItems
 	}
 
-	const contextValue = {all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount, getTotalCardItems}
+	const contextValue = {
+		all_product,
+		cartItems,
+		addToCart,
+		removeFromCart,
+		getTotalCartAmount,
+		getTotalCardItems,
+	}
 	return (
 		<ShopContext.Provider value={contextValue}>
 			{props.children}
