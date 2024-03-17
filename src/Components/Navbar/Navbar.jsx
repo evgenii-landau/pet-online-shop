31adaa2fb5ac81bfda77592090aa24beb99b7168
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-03-17 11:43:04 +0300

    adaptive for header / hero / popular pages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Navbar.module.scss'
 import logo from '../assets/logo.png'
 import cart_icon from '../assets/cart_icon.svg'
+import close_btn from '../assets/close_btn.svg'
 import {Link} from "react-router-dom";
 import {ShopContext} from "../../Context/ShopContext.jsx";
 
@@ -14,7 +15,7 @@ const chosePath = (index) => {
 }
 
 export const Navbar = () => {
-	const [menu, setMenu] = React.useState('Shop')
+	const [isOpenBurger, setIsOpenBurger] = React.useState(false)
 	const {getTotalCardItems} = React.useContext(ShopContext)
 
 	return (
@@ -29,7 +30,7 @@ export const Navbar = () => {
 			{/* Nav-Menu */}
 			<ul className={styles.navMenu}>
 				{menuItems.map((name, index) => (
-					<li onClick={() => setMenu(menuItems[index])} key={index}><Link className={styles.menuLink} to={`${chosePath(index)}`}>{name}</Link></li>
+					<li key={index}><Link className={styles.menuLink} to={`${chosePath(index)}`}>{name}</Link></li>
 				))}
 			</ul>
 
@@ -43,6 +44,36 @@ export const Navbar = () => {
 				</Link>
 				<div className={styles.navCartCount}>{getTotalCardItems()}</div>
 			</div>
+
+			<div onClick={() => setIsOpenBurger(prev => !prev)} className={styles.burger}>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+
+			{/*Menu Burger*/}
+			{isOpenBurger && (
+				<div className={styles.menuBurger}>
+					<button onClick={() => setIsOpenBurger(prev => !prev)} className={styles.closeButton}>
+						<img src={close_btn} alt="close button" width={24} height={24}/>
+					</button>
+					<div className={styles.wrapperBurgerLogin}>
+					<Link to='/login'>
+								<button className={styles.burgerLoginBtn}>Login</button>
+							</Link>
+							<Link to='/cart'>
+								<img src={cart_icon} alt="cart icon" width={50} height={50}/>
+							</Link>
+							<div className={styles.navCartCountBurger}>{getTotalCardItems()}</div>
+					</div>
+					<ul className={styles.menuBurgerList}>
+						{menuItems.map((name, index) => (
+							<li key={index}><Link className={styles.burgerItem} onClick={() => setIsOpenBurger(prev => !prev)} to={`${chosePath(index)}`}>{name}</Link></li>
+						))}
+					</ul>
+				</div>
+			)}
+
 		</div>
 	)
 }
